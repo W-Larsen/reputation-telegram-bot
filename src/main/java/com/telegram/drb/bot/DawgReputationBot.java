df378f6aeafb73f9ce4f3813a6ecb4ede1c642c0
Valentyn <valik.kornienko97@gmail.com>
2020-02-09 14:30:35 +0200

    DRB-1 - Hide bot token in environment variable.

--- src/main/java/com/telegram/drb/bot/DawgReputationBot.java
+++ src/main/java/com/telegram/drb/bot/DawgReputationBot.java
@@ -1,15 +1,24 @@
 package com.telegram.drb.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * Dawg reputation bot implementation.
  *
  * @author Valentyn Korniienko
  */
+@Component
 public class DawgReputationBot extends TelegramLongPollingBot {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DawgReputationBot.class);
 
     @Value("${dawg.reputation.bot.token}")
     private String botToken;
@@ -19,7 +28,22 @@ public class DawgReputationBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(chatId);
 
+            String text = message.getText();
+            response.setText(text);
+
+            try {
+                execute(response);
+                LOGGER.info("Sent message \"{}\" to {}", text, chatId);
+            } catch (TelegramApiException e) {
+                LOGGER.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+            }
+        }
     }
 
     @Override
