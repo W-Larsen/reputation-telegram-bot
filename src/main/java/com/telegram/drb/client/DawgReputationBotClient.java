2db877671992cb4527fa0f640c286ca3b522cf12
Valentyn Korniienko <valik.kornienko97@gmail.com>
2020-02-14 22:31:53 +0200

    DRB-3 - Add Client to scheduled ping application

---
+++ src/main/java/com/telegram/drb/client/DawgReputationBotClient.java
@@ -0,0 +1,34 @@
+package com.telegram.drb.client;
+
+import com.telegram.drb.model.rest.PingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Dawg reputation bot client.
+ *
+ * @author Valentyn Korniienko
+ */
+@Component
+public class DawgReputationBotClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${dawg.ping.url}")
+    private String url;
+
+    /**
+     * Ping.
+     *
+     * @return ping response
+     */
+    public PingResponse ping() {
+        ResponseEntity<PingResponse> response = restTemplate.getForEntity(url + "/ping", PingResponse.class);
+        return response.getBody();
+    }
+
+}
---
+++ src/main/java/com/telegram/drb/model/domain/PingStatus.java
@@ -0,0 +1,11 @@
+package com.telegram.drb.model.domain;
+
+/**
+ * Ping status enum.
+ */
+public enum PingStatus {
+
+    AVAILABLE,
+
+    UUAVAILABLE
+}
